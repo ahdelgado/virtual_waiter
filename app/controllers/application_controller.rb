254a36ce9945a9456c0e8378f9b1275d49bc70a7
Alcides Ruiz <alcides.ruiz@genalyte.com>
2020-07-16 19:03:36 -0400

    Skip  verify_authenticity_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
