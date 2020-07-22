9f05ce01b8456f2f6f85b9efb2177539a1f94f2f
Alcides Ruiz <alcides.ruiz@genalyte.com>
2020-07-22 19:01:06 -0400

    Add deleted_at to users and fix typo bug in clients#show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_010607) do
+ActiveRecord::Schema.define(version: 2020_07_22_225438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -242,7 +242,9 @@ ActiveRecord::Schema.define(version: 2020_07_22_010607) do
     t.string "name"
     t.string "phone_number"
     t.boolean "admin"
+    t.datetime "deleted_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
